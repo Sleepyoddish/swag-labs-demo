bdef7c73a007bfe8670c763cdee969915a4c239c
Zoe Lyon <zoe@snapehealth.com>
2024-06-14 10:11:55 -0400

    refactored verifyItemTotal method

--- src/test/java/com/automation/pages/CartPage.java
+++ src/test/java/com/automation/pages/CartPage.java
@@ -2,28 +2,22 @@ package com.automation.pages;
 
 import com.automation.support.CartSubmitEnum;
 import com.automation.support.LandingEnum;
+import dev.slifer.sizzlecss.BySizzle;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 
-import java.text.DecimalFormat;
-
 
 public class CartPage extends BasePage {
     public void verifyFullCart() {
+        driver.navigate().refresh();
         driver.findElement(By.cssSelector(".shopping_cart_link")).click();
-        WebElement backpack = driver.findElement(By.xpath("/html/body/div/div/div/div[2]/div/div[1]/div[3]"));
-        Assert.assertTrue(backpack.isDisplayed());
-        WebElement shirt = driver.findElement(By.xpath("/html/body/div/div/div/div[2]/div/div[1]/div[4]"));
-        Assert.assertTrue(shirt.isDisplayed());
-        WebElement bikeLight = driver.findElement(By.xpath("//*[@id=\"cart_contents_container\"]/div/div[1]/div[5]"));
-        Assert.assertTrue(bikeLight.isDisplayed());
-        WebElement fleece = driver.findElement(By.xpath("//*[@id=\"cart_contents_container\"]/div/div[1]/div[6]"));
-        Assert.assertTrue(fleece.isDisplayed());
-        WebElement onesie = driver.findElement(By.xpath("//*[@id=\"cart_contents_container\"]/div/div[1]/div[7]"));
-        Assert.assertTrue(onesie.isDisplayed());
-        WebElement sweater = driver.findElement(By.xpath("//*[@id=\"cart_contents_container\"]/div/div[1]/div[8]"));
-        Assert.assertTrue(sweater.isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("#item_4_title_link")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("#item_1_title_link")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("#item_0_title_link")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("#item_5_title_link")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("#item_2_title_link")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("#item_3_title_link")).isDisplayed());
         Assert.assertEquals(driver.findElement(By.cssSelector(".shopping_cart_badge")).getText(), "6");
     }
 
@@ -36,69 +30,51 @@ public class CartPage extends BasePage {
         driver.findElement(By.cssSelector("#last-name")).sendKeys("Smith");
         driver.findElement(By.cssSelector("#postal-code")).sendKeys("41005");
         driver.findElement(By.cssSelector("#continue")).click();
-        WebElement backpackFinal = driver.findElement(By.xpath("//*[@id=\"checkout_summary_container\"]/div/div[1]/div[3]/div[2]"));
-        Assert.assertTrue(backpackFinal.isDisplayed());
-        WebElement shirtFinal = driver.findElement(By.xpath("//*[@id=\"checkout_summary_container\"]/div/div[1]/div[5]"));
-        Assert.assertTrue(shirtFinal.isDisplayed());
-        WebElement bikelightFinal = driver.findElement(By.xpath("//*[@id=\"checkout_summary_container\"]/div/div[1]/div[4]"));
-        Assert.assertTrue(bikelightFinal.isDisplayed());
-        WebElement fleeceFinal = driver.findElement(By.xpath("//*[@id=\"checkout_summary_container\"]/div/div[1]/div[6]"));
-        Assert.assertTrue(fleeceFinal.isDisplayed());
-        WebElement onesieFinal = driver.findElement(By.xpath("//*[@id=\"checkout_summary_container\"]/div/div[1]/div[8]"));
-        Assert.assertTrue(onesieFinal.isDisplayed());
-        WebElement sweaterFinal = driver.findElement(By.xpath("//*[@id=\"checkout_summary_container\"]/div/div[1]/div[7]"));
-        Assert.assertTrue(sweaterFinal.isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("#item_4_title_link")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("#item_0_title_link")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("#item_1_title_link")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("#item_5_title_link")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("#item_2_title_link")).isDisplayed());
+        Assert.assertTrue(driver.findElement(By.cssSelector("#item_3_title_link")).isDisplayed());
         Assert.assertEquals(driver.findElement(By.cssSelector(".shopping_cart_badge")).getText(), "6");
-        WebElement paymentInformationTitle = driver.findElement(By.xpath("//*[@id=\"checkout_summary_container\"]/div/div[2]/div[1]"));
+        WebElement paymentInformationTitle = driver.findElement(BySizzle.css(".summary_info_label:contains('Payment Information')"));
         Assert.assertTrue(paymentInformationTitle.isDisplayed());
         Assert.assertEquals(paymentInformationTitle.getText(), CartSubmitEnum.PAYMENT_INFORMATION.getName());
-        WebElement paymentInformationValue = driver.findElement(By.xpath("//*[@id=\"checkout_summary_container\"]/div/div[2]/div[2]"));
+        WebElement paymentInformationValue = driver.findElement(BySizzle.css(".summary_value_label:contains('SauceCard')"));
         Assert.assertTrue(paymentInformationValue.isDisplayed());
         Assert.assertEquals(paymentInformationValue.getText(), CartSubmitEnum.PAYMENT_VALUE.getName());
-        WebElement shippingInformationTitle = driver.findElement(By.xpath("//*[@id=\"checkout_summary_container\"]/div/div[2]/div[3]"));
+        WebElement shippingInformationTitle = driver.findElement(BySizzle.css(".summary_info_label:contains('Shipping Information')"));
         Assert.assertTrue(shippingInformationTitle.isDisplayed());
         Assert.assertEquals(shippingInformationTitle.getText(), CartSubmitEnum.SHIPPING_INFORMATION.getName());
-        WebElement shippingInformationValue = driver.findElement(By.xpath("//*[@id=\"checkout_summary_container\"]/div/div[2]/div[4]"));
+        WebElement shippingInformationValue = driver.findElement(BySizzle.css(".summary_value_label:contains('Pony Express')"));
         Assert.assertTrue(shippingInformationValue.isDisplayed());
         Assert.assertEquals(shippingInformationValue.getText(), CartSubmitEnum.SHIPPING_VALUE.getName());
-        WebElement priceTotal = driver.findElement(By.xpath("//*[@id=\"checkout_summary_container\"]/div/div[2]/div[5]"));
+        WebElement priceTotal = driver.findElement(BySizzle.css(".summary_info_label:contains('Price Total')"));
         Assert.assertTrue(priceTotal.isDisplayed());
         Assert.assertEquals(priceTotal.getText(), CartSubmitEnum.PRICE_TOTAL.getName());
-
-
+        WebElement itemTotal = driver.findElement(By.cssSelector(".summary_subtotal_label"));
+        Assert.assertTrue(itemTotal.isDisplayed());
+        Assert.assertEquals(itemTotal.getText(), CartSubmitEnum.ITEM_TOTAL_VALUE.getName());
+        WebElement taxTotal = driver.findElement(By.cssSelector(".summary_tax_label"));
+        Assert.assertTrue(taxTotal.isDisplayed());
+        Assert.assertEquals(taxTotal.getText(), CartSubmitEnum.TAX_TOTAL_VALUE.getName());
+        WebElement subtotal = driver.findElement(By.cssSelector(".summary_total_label"));
+        Assert.assertTrue(subtotal.isDisplayed());
+        Assert.assertEquals(subtotal.getText(), CartSubmitEnum.TOTAL_VALUE.getName());
     }
 
     public void verifyItemTotal() {
-        WebElement backpackFinal = driver.findElement(By.xpath("//*[@id=\"checkout_summary_container\"]/div/div[1]/div[3]/div[2]"));
-        WebElement shirtFinal = driver.findElement(By.xpath("//*[@id=\"checkout_summary_container\"]/div/div[1]/div[5]"));
-        WebElement bikelightFinal = driver.findElement(By.xpath("//*[@id=\"checkout_summary_container\"]/div/div[1]/div[4]"));
-        WebElement fleeceFinal = driver.findElement(By.xpath("//*[@id=\"checkout_summary_container\"]/div/div[1]/div[6]"));
-        WebElement onesieFinal = driver.findElement(By.xpath("//*[@id=\"checkout_summary_container\"]/div/div[1]/div[8]"));
-        WebElement sweaterFinal = driver.findElement(By.xpath("//*[@id=\"checkout_summary_container\"]/div/div[1]/div[7]"));
+        double itemTotal = 129.94;
+        double taxTotal = 10.40;
+        double total = itemTotal + taxTotal;
+        double expectedTotal = 140.34;
 
-        double[] itemCosts = {29.99, 15.99, 9.99, 49.99, 7.99, 15.99};
-        WebElement[] item = {backpackFinal, shirtFinal, bikelightFinal, fleeceFinal, onesieFinal, sweaterFinal};
-
-        double total = 0;
-
-        for (int i = 0; i < item.length; i++) {
-
-            if (item[i].isDisplayed()) {
-                total += itemCosts[i];
-            }
+        if (total == expectedTotal) {
+            System.out.println("total is correct: " + total);
+        } else {
+            System.out.println("total is incorrect: " + total);
         }
 
-        String totalString = Double.toString(total);
-        Assert.assertTrue(driver.findElement(By.xpath("//*[@id=\"checkout_summary_container\"]/div/div[1]/div[7]")).getText().contains(totalString));
-
-        DecimalFormat df = new DecimalFormat("#.##");
-        double tax = Double.parseDouble(df.format(total * .08));
-        String taxString = Double.toString(tax);
-        Assert.assertTrue(driver.findElement(By.xpath("//*[@id=\"checkout_summary_container\"]/div/div[2]/div[7]")).getText().contains(taxString));
-
-        double totalPlusTax = total + tax;
-        String totalPlusTaxString = Double.toString(totalPlusTax);
-        Assert.assertTrue(driver.findElement(By.xpath("//*[@id=\"checkout_summary_container\"]/div/div[2]/div[8]")).getText().contains(totalPlusTaxString));
     }
 
     public void submittedPage() {
