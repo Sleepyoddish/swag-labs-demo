c2d311e8ab13842961e5e2c571a610af936e0ecb
Zoe Lyon <zoe@snapehealth.com>
2024-06-14 10:14:38 -0400

    added wait

--- src/test/java/com/automation/pages/BasePage.java
+++ src/test/java/com/automation/pages/BasePage.java
@@ -1,9 +1,17 @@
 package com.automation.pages;
 
+import com.automation.support.DriverUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class BasePage {
     WebDriver driver;
     WebDriverWait wait;
+
+    public BasePage() {
+        driver = DriverUtils.getDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+    }
 }
